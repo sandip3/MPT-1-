84893722a74ee206f2b911b36942619cd5d3fec2
sandip3 <sandipmishra55555.com>
2023-05-27 19:06:46 +0530

    [MPT] Project 2 For Loop With Arrys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
-  email: string = "";
+    title = 'myApp';
+    email: string = "";
     password: string = "";
     viewMode: string = "";
+    tasks = ['MI', 'Samsung', 'Iphone', 'Google'];
 
   submit() {
     console.log(this.email);
     console.log(this.password);
     debugger
   }
+    
 }
